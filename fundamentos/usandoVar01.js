c781ad85cd1e4fcb274b66b3d09f15b158917765
retpinheiro <reehpinheiro.s@gmail.com>
2022-07-27 10:28:19 -0300

    atualizacao

--- fundamentos/usandoVar01.js
+++ fundamentos/usandoVar01.js
@@ -1,3 +1,17 @@
-{ { { var sera = 'Será' } } }
+{
+    {
+        {
+            var sera = 'Será'
+            console.log(sera)
+        }
+    }
+}
 
 console.log(sera);
+
+function teste(){
+    var local = 123;
+}
+
+teste();
+console.log(local);
