2f844f650f24e85c25642a4737f051ad3314da56
retpinheiro <reehpinheiro.s@gmail.com>
2022-07-28 09:44:35 -0300

    atualizar

--- fundamentos/notacaoPonto.js
+++ fundamentos/notacaoPonto.js
@@ -2,3 +2,14 @@ console.log(Math.ceil(6.1));
 
 const obj1 = {}
 obj1.nome = 'Bola';
+// obj1['nome'] = 'Bola2';
+console.log(obj1.nome);
+
+function Obj(nome) {
+    this.nome = nome;
+}
+
+const Obj2 = new Obj('Cadeira');
+const Obj3 = new Obj('Mesa');
+console.log(obj2.nome);
+console.log(obj3.nome);
